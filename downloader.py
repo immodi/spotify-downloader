b09d9939d5af27faebd5a2bc9bc97c7d57f9cedf
immodi <ay727467@gmail.com>
2023-11-24 00:06:39 +0200

    add author and thumbnail

--- downloader.py
+++ downloader.py
@@ -4,13 +4,21 @@ import os
 
 
 ydl_opts = {
+    'writethumbnail': True,
     'format': 'bestaudio/best',
     'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }],
-    'ffmpeg-location': "ffmpeg",
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        },{
+            'key': 'FFmpegMetadata',
+            'add_metadata': True,
+        },{
+            'key': 'EmbedThumbnail',
+            'already_have_thumbnail': True
+        }
+    ],
+    'ffmpeg-location': "ffmpeg.exe",    
 }
 
 def download_with_link(link, save_path):
@@ -19,10 +27,14 @@ def download_with_link(link, save_path):
     except OSError as error:
         print(error)   
     ydl_opts['outtmpl'] = save_path + '/%(title)s.%(ext)s'
-
+    
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         if type(link) == str:
+            # info = ydl.extract_info(link, True)
+            # embed_cover_art(info["title"])
             ydl.download([link])
-        elif type(link) == list:
-            ydl.download(link)
+        # elif type(link) == list:
+        #     ydl.download(link)
         else: return None
+
+
